b1ba55d91db1f3ec881b28fbbd29ab268d9ade30
tayutaedomo <tayutaedomo@gmail.com>
2023-11-26 18:30:18 +0900

    Section 2 Lesson 3 is completed

---
+++ src/scripts/3-config-nft.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+
+import sdk from "./1-initialize-sdk";
+import { editionDropAddress } from "./module";
+
+const editionDrop = sdk.getContract(editionDropAddress!, "edition-drop");
+
+(async () => {
+  try {
+    await (
+      await editionDrop
+    ).createBatch([
+      {
+        name: "Member's symbol",
+        description: "FooBar DAO にアクセスすることができる限定アイテムです",
+        image: readFileSync("src/scripts/assets/NFT.png"),
+      },
+    ]);
+    console.log("✅ Successfully created a new NFT in the drop!");
+  } catch (error) {
+    console.error("failed to create the new NFT", error);
+  }
+})();
